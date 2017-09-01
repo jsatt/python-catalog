295e0b78d095c5dff3cfb75dc45c029c948abe42
Jeremy Satterfield <jsatt22@gmail.com>
2017-09-01 11:16:46 -0500

    make compat with py2
    I know. Wrong direction.

--- catalog.py
+++ catalog.py
@@ -1,17 +1,61 @@
-__version__ = '1.1.1'
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
+from future import standard_library
+standard_library.install_aliases()
+
+import sys
+import inspect
 from collections import OrderedDict
+from functools import wraps
 from itertools import zip_longest
 
+from future.utils import with_metaclass
 
-class CatalogMeta:
+
+class Prepareable(type):
+    # modified from https://gist.github.com/DasIch/5562625#file-prepareable-py
+    if sys.version_info < (3,):
+        def __new__(cls, name, bases, attributes):
+            try:
+                constructor = attributes["__new__"]
+            except KeyError:
+                return type.__new__(cls, name, bases, attributes)
+
+            def preparing_constructor(cls, name, bases, attributes):
+                try:
+                    cls.__prepare__
+                except AttributeError:
+                    return constructor(cls, name, bases, attributes)
+                namespace = cls.__prepare__.im_func(cls, name, bases)
+                defining_frame = sys._getframe(1)
+                def get_index(*args, **kwargs):
+                    return 0
+                for constant in reversed(defining_frame.f_code.co_consts):
+                    if inspect.iscode(constant) and constant.co_name == name:
+                        def get_index(attribute_name, _names=constant.co_names):
+                            try:
+                                return _names.index(attribute_name)
+                            except ValueError:
+                                return 0
+                        break
+                by_appearance = sorted(
+                    attributes.items(), key=lambda item: get_index(item[0])
+                )
+                for key, value in by_appearance:
+                    namespace[key] = value
+                return constructor(cls, name, bases, namespace)
+            attributes["__new__"] = wraps(constructor)(preparing_constructor)
+            return type.__new__(cls, name, bases, attributes)
+
+
+class CatalogMeta(with_metaclass(Prepareable, type)):
     @classmethod
-    def __prepare__(metacls, cls, bases):
+    def __prepare__(metacls, name, bases, **kwargs):
         return OrderedDict()  # keep members in order which they are set
 
-    @staticmethod
-    def __new__(cls, name, bases, newattrs):
-        kls = super().__new__(cls)
+    def __new__(metaname, clsname, bases, newattrs):
+        kls = type.__new__(metaname, clsname, bases, newattrs)
         kls._member_map_ = OrderedDict()
         kls._member_names_ = []
         member_cls = newattrs.get('_member_class', CatalogMember)
@@ -47,7 +91,7 @@ class CatalogMeta:
 
     def __delattr__(cls, attr):
         if attr in cls._member_map_:
-            raise AttributeError("cannot delete member")
+            raise AttributeError("cannot delete member '{}'".format(attr))
         super().__delattr__(attr)
 
     def _zip(cls, *attrs):
@@ -57,7 +101,7 @@ class CatalogMeta:
                 for m in cls._member_map_.values())
 
 
-class CatalogMember:
+class CatalogMember(object):
     def __init__(self, name, keys, values):
         if len(keys) == 1 and not isinstance(values, (list, tuple, set)):
             values = [values]
@@ -74,7 +118,7 @@ class CatalogMember:
         return "%s.%s" % (self.__class__.__name__, self.name)
 
 
-class Catalog(metaclass=CatalogMeta):
+class Catalog(with_metaclass(CatalogMeta, object)):
     """
     Structure for complex enumeration
 
--- setup.py
+++ setup.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python
 
-import sys
-if sys.version_info < (3,):
-    print('This package requires Python 3.')
-    sys.exit(1)
-
 from distutils.core import setup
-
-import catalog
 
 long_description = open('README').read()
 
 setup_args = dict(
     name='pycatalog',
-    version=catalog.__version__,
+    version='1.1.1',
     description='Data structure for complexe enumeration.',
     long_description=long_description,
     author='Jeremy Satterfield',
     author_email='jsatt@jsatt.com',
     url='https://github.com/jsatt/python-catalog',
     license="MIT License",
+    install_requires=[
+        'future'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -27,7 +22,8 @@ setup_args = dict(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development',
     ],
     py_modules=['catalog'],
